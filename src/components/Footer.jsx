2399cf5f0054bbca34298085b68fc20b2ccdc55a
Kévin GRANIER <kevin.granier@gmail.com>
2023-03-30 16:05:42 +0200

    #5 Footer
    Add Footer, desktop integration

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className="background">
+            <img src="./img/background.jpg" alt="" />
+        </div>
+        <ul>
+            <li><a href="/"><img src="./logo/contact.png" alt="" /></a></li>
+            <li><a href="/"><img src="./logo/facebook.png" alt="" /></a></li>
+            <li><a href="/"><img src="./logo/twiter.png" alt="" /></a></li>
+            <li><a href="/"><img src="./logo/pinterest.png" alt="" /></a></li>
+            <li><a href="/"><img src="./logo/linkedin.png" alt="" /></a></li>
+        </ul> 
+    </footer>
+  )
+}
