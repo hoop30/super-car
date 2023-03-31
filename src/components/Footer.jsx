0565836b0145a6603e1ef5bce7cc96798af8d187
Kévin GRANIER <kevin.granier@gmail.com>
2023-03-31 10:14:10 +0200

    #10 Pages, Footer
    All Pages and components 100% responsive
    Footer icons color on hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
             <img src="./img/background.jpg" alt="" />
         </div>
         <ul>
-            <li><a href="/"><img src="./logo/contact.png" alt="" /></a></li>
-            <li><a href="/"><img src="./logo/facebook.png" alt="" /></a></li>
-            <li><a href="/"><img src="./logo/twiter.png" alt="" /></a></li>
-            <li><a href="/"><img src="./logo/pinterest.png" alt="" /></a></li>
-            <li><a href="/"><img src="./logo/logo linkedin.png" alt="" /></a></li>
+            <li><a href="/"><img src="./logo/contact.png" alt="" width='50' height='50'/></a></li>
+            <li><a href="/"><img src="./logo/facebook.png" alt="" width='50' height='50'/></a></li>
+            <li><a href="/"><img src="./logo/twitter.png" alt="" width='50' height='50'/></a></li>
+            <li><a href="/"><img src="./logo/pinterest.png" alt="" width='50' height='50'/></a></li>
+            <li><a href="/"><img src="./logo/linkedin.png" alt="" width='50' height='50'/></a></li>
         </ul> 
     </footer>
   )
